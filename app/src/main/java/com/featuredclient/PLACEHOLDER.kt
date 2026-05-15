62d2d95eb3cf923b114b6b03daa652c9e68ded30
DevOP-clouder <vidyakb999@gmail.com>
2026-05-16 00:26:51 +0530

    Delete app/src/main/java/com/featuredclient directory

--- app/src/main/java/com/featuredclient/PLACEHOLDER.kt
+++
@@ -1,6 +0,0 @@
-package com.featuredclient
-
-/**
- * Placeholder file to establish initial package namespace for Featured Client.
- */
-object FeaturedClientPlaceholder
--- app/src/main/java/com/featuredclient/update/VersionComparator.kt
+++
@@ -1,9 +0,0 @@
-package com.featuredclient.update
-
-import com.featuredclient.update.model.UpdateManifest
-
-object VersionComparator {
-    fun hasUpdate(currentVersionCode: Int, remote: UpdateManifest): Boolean {
-        return remote.versionCode > currentVersionCode
-    }
-}
